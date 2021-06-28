5da2a579ce478e9ebaa2ab542d942c314de5b404
Joshua Lopez <joshualopez0429@gmail.com>
2021-06-28 18:12:14 +0000

    Adding backup folder for developing scripts

---
+++ development/main.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import json, torch, glob
+import numpy as np
+from torch import nn
+from torchvision import datasets
+from torchvision.transforms import ToTensor, Lambda
+
+if __name__ == "__main__":
+    while (True):
+        {
+            model.load
+        }
+
